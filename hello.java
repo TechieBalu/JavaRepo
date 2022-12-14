89a35207b8e899dea41e6ab858ac2ac8a90d4a9a
TechieBalu <120528675+TechieBalu@users.noreply.github.com>
2022-12-14 05:55:06 +0530

    Create hello.java
    Test first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+package com.balaji.org;
+
+public class HelloBalaji {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Balaji");
+
+	}
+
+}
